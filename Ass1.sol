d5b4c34dbcdc91b332cb5cf0762421f713a9204e
us579 <wzus579@gmail.com>
2019-03-07 12:11:03 +1100

    commit

--- Ass1.sol
+++ Ass1.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.4.22;
 
-contract MakeChoices {
+/// @title 委托投票
+contract Ballot {
     // 这里声明了一个新的复合类型用于稍后的变量
     // 它用来表示一个选民
-    
-    uint private quorum;
-    uint count=0;
-    
+        
     bytes32[] public arr;
-    bytes32[] public arr1;
+    bool public ended;
+    uint public quorum;
     
     
-
     struct Voter {
-        uint weight; // 计票的权重，也就是票数
+        uint weight; // 计票的权重
         bool voted;  // 若为真，代表该人已投票
+        address delegate; // 被委托人
         uint vote;   // 投票提案的索引
     }
 
@@ -27,25 +26,14 @@ contract MakeChoices {
     address public chairperson;
 
     // 这声明了一个状态变量，为每个可能的地址存储一个 `Voter`。
-    // 有权利投票的用户map
     mapping(address => Voter) public voters;
 
     // 一个 `Proposal` 结构类型的动态数组
-    // 投票提案数组
     Proposal[] public proposals;
-    
-    
-    function getQuorum(uint quorum_){
-        require(msg.sender == chairperson );
-        quorum = quorum_;
-    }
 
-    // 为 `proposalNames` 中的每个提案，创建一个提案对象
-    // 合约的构造函数，用constructor标识
+    /// 为 `proposalNames` 中的每个提案，创建一个新的（投票）表决
     function add_proposal(bytes32[] proposalNames) public {
-        // 合约的构造者为chairperson
         chairperson = msg.sender;
-        // 赋予投票的权利
         voters[chairperson].weight = 1;
         //对于提供的每个提案名称，
         //创建一个新的 Proposal 对象并把它添加到数组的末尾。
@@ -58,10 +46,15 @@ contract MakeChoices {
             }));
         }
     }
-
-    // 授权 `voter` 投票的权利
+    
+    function getQuorum(uint quorum_){
+        require(msg.sender == chairperson );
+        quorum = quorum_;
+    }
+    
+    // 授权 `voter` 对这个（投票）表决进行投票
     // 只有 `chairperson` 可以调用该函数。
-    function giveRightToVote(address voter)  public {
+    function giveRightToVote(address voter) public {
         // 若 `require` 的第一个参数的计算结果为 `false`，
         // 则终止执行，撤销所有对状态和以太币余额的改动。
         // 在旧版的 EVM 中这曾经会消耗所有 gas，但现在不会了。
@@ -75,28 +68,37 @@ contract MakeChoices {
             !voters[voter].voted,
             "The voter already voted."
         );
-       // require(voters[voter].weight == 0);
+        require(voters[voter].weight == 0);
         voters[voter].weight = 1;
     }
 
+    function fet() public view returns(uint num){
+        num = proposals.length;
+    }
 
 
+    function sumall() public view returns (uint) {
+        uint S;
+        for(uint i;i < proposals.length;i++){
+            S += proposals[i].voteCount;
+        }
+        return S;
+    }
+    
     /// 把你的票(包括委托给你的票)，
     /// 投给提案 `proposals[proposal].name`.
     function vote(uint proposal) public {
-        for (uint p = 0; p < proposals.length; p++) {
-            count += proposals[p].voteCount;
-            }
-        require(count < quorum,"exceed quorum");
+        require(sumall() < quorum,'exceed quorum,vote terminate' );
         Voter storage sender = voters[msg.sender];
         require(!sender.voted, "Already voted.");
         sender.voted = true;
         sender.vote = proposal;
         // 如果 `proposal` 超过了数组的范围，则会自动抛出异常，并恢复所有的改动
         proposals[proposal].voteCount += sender.weight;
+        if (sumall() ==  quorum){
+            ended = true;
+        }
     }
-    
-    
     
     function list_all_choices() public view 
         returns(bytes32[]){
@@ -106,23 +108,8 @@ contract MakeChoices {
         return arr;
     }
     
-
-    
-    function getResult() public view 
-        returns(bytes32 name,uint voteCount){
-       for (uint p = 0; p < proposals.length; p++){
-             //arr1.push(proposals[p].name);
-             return  (proposals[p].name,proposals[p].voteCount);
-        }
-       
-    }
-    
-    function kill() public {
-        require(msg.sender == chairperson);
-        selfdestruct(msg.sender);
-    }
-
-   function winningProposal() public view
+    /// @dev 结合之前所有的投票，计算出最终胜出的提案
+    function winningProposal() public view
             returns (uint winningProposal_)
     {
         uint winningVoteCount = 0;
@@ -139,5 +126,12 @@ contract MakeChoices {
             returns (bytes32 winnerName_)
     {
         winnerName_ = proposals[winningProposal()].name;
+    }
+
+
+    function destory_contract() public {
+        require(ended,"Contract only can be destoryed after poll");
+        require(msg.sender == chairperson,"Only cractor can destory the Contract");
+        selfdestruct(msg.sender);
     }
 }
