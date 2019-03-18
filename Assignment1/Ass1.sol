00db70ee82c62ed1a62371a5cfb8635402a1bd28
us579 <wzus579@gmail.com>
2019-03-18 15:34:05 +1100

    commit

--- Assignment1/Ass1.sol
+++ Assignment1/Ass1.sol
@@ -1,42 +1,46 @@
 pragma solidity ^0.4.22;
 
+///contract address: 0xEBfd3872143AF032D2df23019d03cf7797A37c7C
+///initial with quorum == 3
+
 /// @title Poll Smart Contract
 contract Poll {
-
     //the array stores all the chonices name which provided the person who call the `list_all_choice` function
-    bytes32[] public arr;
+    bytes32[] private arr;
     //the variable represent whether the poll finised
-    bool public ended;
-    uint public quorum;
-    uint[] public arrVote;
+    bool private ended;
+    //the variable represent the max votes
+    uint private quorum;
+    //the array to store votes
+    uint[] private arrVote;
 
-
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
+    
+    //the struct represent a single Voter.
     struct Voter {
         uint weight; // weight is accumulated by delegation
         bool voted;  // if true, that person already voted
         uint vote;   // index of the voted choices
     }
 
+
     // the details of choice
     struct Choice {
         bytes32 name;   // choice name（32bytes）
         uint vcount; // the number of vote
     }
-
-    address public creator;
+    
+    //the address of the depolyer
+    address private creator;
 
     // This declares a state variable that
     // stores a `Voter` struct for each possible address.
-    mapping(address => Voter) public voters;
+    mapping(address => Voter) private voters;
 
     // A dynamically-sized array of `Choice` structs.
-    Choice[] public choices;
+    Choice[] private choices;
 
-    /// 为 `proposalNames` 中的每个提案，创建一个新的（投票）表决
-    function add_proposal(bytes32[] ChoiceNames) public {
+    //allow depolyer to add more choices.
+    function add_chocies(bytes32[] ChoiceNames) public {
         creator = msg.sender;
         voters[creator].weight = 1;
         for (uint i = 0; i < ChoiceNames.length; i++) {
@@ -47,14 +51,13 @@ contract Poll {
         }
     }
 
-    //nput the  number of quorum
-    function getQuorum(uint quorum_){
-        require(msg.sender == creator);
+    //input the number of quorum when deplying the contract, initial with 0
+    constructor (uint quorum_) public {
         quorum = quorum_;
     }
 
-    // Give `voter` the right to vote on this poll.
-    // only  `chairperson` can implements this function
+    // Give voter the right to vote on this poll.
+    // only chairperson can implements this function
     function giveRightToVote(address voter) public {
         require(msg.sender == creator,"Only creator can add friends to voter." );
         require(!voters[voter].voted,"The voter already voted.");
@@ -68,7 +71,7 @@ contract Poll {
     }
 
     // function for calculating the number of the voting so far and return the sum
-    function sumall() public view returns (uint) {
+    function sumall() private view returns (uint) {
         uint S;
         for(uint i;i < choices.length;i++){
             S += choices[i].vcount;
@@ -98,16 +101,16 @@ contract Poll {
         }
         return arr;
     }
-
-    function getResult() public view returns(uint[],bytes32[]){
+    //function to get poll result so far(this is the second methods to get result)
+    function getResult2() private view returns(uint[],bytes32[]){
         for (uint p = 0; p < choices.length; p++) {
             arrVote.push(choices[p].vcount);
         }
         return (arrVote,list_all_choices());
     }
     
-    
-    function getarry() external view returns (bytes32[] memory name, uint[] memory vcount) {
+    //function to get poll result so far
+    function getResult() external view returns (bytes32[] memory name, uint[] memory vcount) {
         name = new bytes32[](choices.length);
         vcount = new uint[](choices.length);
         for(uint i = 0; i<choices.length;i++) {
